5840b5eae3998cfc93f7468ab9e9dac5c46f6d49
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-16 08:57:39 +0200

    Add comments to RST processor

--- handsdown/processors/rst.py
+++ handsdown/processors/rst.py
@@ -9,25 +9,35 @@ class RSTDocstringProcessor(BaseDocstringProcessor):
     """
 
     line_re_map = {
+        # PEP 287 arg typed with description
         re.compile(
             r"^:(?P<section>param|parameter)\s+(?P<type>\w+)\s+(?P<param>\w+)\s*:\s*(?P<desc>.+)$"
         ): "- `{param}` *{type}* - {desc}",
+        # PEP 287 arg with description
         re.compile(
             r"^:(?P<section>param|parameter)\s+(?P<param>\w+)\s*:\s*(?P<desc>.+)$"
         ): "- `{param}` - {desc}",
+        # PEP 287 arg typed
         re.compile(
             r"^:(?P<section>param|parameter)\s+(?P<type>\w+)\s+(?P<param>\w+)\s*:$"
         ): "- `{param}` *{type}*",
+        # PEP 287 arg
         re.compile(
             r"^:(?P<section>param|parameter)\s+(?P<param>\w+)\s*:$"
         ): "- `{param}`",
+        # PEP 287 return
         re.compile(r":(?P<section>returns?)\s*:\s*(?P<desc>.*)?$"): "{desc}",
+        # PEP 287 return typed
         re.compile(r":(?P<section>returns?)\s+(?P<type>[^:]+):$"): "Type: *{type}*",
+        # PEP 287 return typed with description
         re.compile(
             r":(?P<section>returns?)\s+(?P<type>[^:]+)\s*:\s*(?P<desc>.+)$"
         ): "Type: *{type}*\n{desc}",
+        # PEP 287 rtype
         re.compile(r":(?P<section>rtype)\s*:\s+(?P<type>[^:]+)$"): "Type: *{type}*",
+        # PEP 287 raises typed
         re.compile(r":(?P<section>raises?)\s+(?P<type>\w+)\s*:$"): "- `{type}`",
+        # PEP 287 raises typed with description
         re.compile(
             r":(?P<section>raises?)\s+(?P<type>\w+)\s*:(?P<desc>.+)$"
         ): "- `{type}` - {desc}",
