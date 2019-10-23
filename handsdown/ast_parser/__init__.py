0f9644cb05e6c07bd1e75f8c1fdfbeaf3c422a8e
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-23 05:31:07 +0200

    Remove typed-ast

--- handsdown/ast_parser/__init__.py
+++ handsdown/ast_parser/__init__.py
@@ -1,11 +1,4 @@
 import sys
+import ast
 
 ast_version = sys.version_info.major
-
-try:
-    if sys.version_info.major == 2:
-        import typed_ast.ast2 as ast
-    else:
-        import typed_ast.ast3 as ast  # type: ignore
-except ImportError:
-    import ast  # type: ignore
