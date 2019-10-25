fd1479d4d626dc669b8e34b77192d82283d07072
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-25 13:47:36 +0200

    Add dockerize script

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=`handsdown -v`
+IMAGE_NAME="handsdown"
+VERSION=`python -m handsdown -v`
 
-docker build --tag handsdown --tag handsdown:latest --tag handsdown:$VERSION .
+docker build --tag ${IMAGE_NAME} .
+
+# docker login docker.pkg.github.com --username vemel -p $GITHUB_TOKEN
+
+docker tag handsdown docker.pkg.github.com/vemel/handsdown/${IMAGE_NAME}:latest
+
+docker push docker.pkg.github.com/vemel/handsdown/${IMAGE_NAME}:latest
