50d9e4854dc6fa609d3d35e1276491f47b298bc8
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-27 21:10:24 +0100

    Add unit tests for import string

---
+++ tests/utils/test_import_string.py
@@ -0,0 +1,46 @@
+# pylint: disable=missing-docstring
+import unittest
+
+from handsdown.utils.import_string import ImportString, ImportStringError
+
+
+class TestImportString(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(ImportString("value").value, "value")
+        self.assertEqual(str(ImportString("value")), "value")
+        self.assertTrue(hash(ImportString("value")))
+        self.assertTrue(ImportString("value"))
+        self.assertFalse(ImportString(""))
+        self.assertEqual((ImportString("value") + "add").value, "value.add")
+        self.assertEqual((ImportString("") + "add").value, "add")
+        self.assertEqual(ImportString("value"), ImportString("value"))
+        self.assertNotEqual(ImportString("value"), "value")
+        self.assertEqual(
+            ImportString("parent.parent2.value").parent.value, "parent.parent2"
+        )
+
+        with self.assertRaises(ImportStringError):
+            _ = ImportString("value").parent
+
+    def test_is_top_level(self):
+        self.assertTrue(ImportString("value").is_top_level())
+        self.assertTrue(ImportString("").is_top_level())
+        self.assertFalse(ImportString("parent.value").is_top_level())
+
+    def test_startswith(self):
+        self.assertTrue(
+            ImportString("parent.parent2.value").startswith(ImportString("parent"))
+        )
+        self.assertTrue(
+            ImportString("parent.parent2.value").startswith(
+                ImportString("parent.parent2")
+            )
+        )
+        self.assertFalse(
+            ImportString("parent.parent2.value").startswith(ImportString("parent2"))
+        )
+        self.assertFalse(
+            ImportString("parent.parent2.value").startswith(
+                ImportString("parent.parent2value")
+            )
+        )
--- tests/utils/test_path_finder.py
+++ tests/utils/test_path_finder.py
@@ -1,11 +1,30 @@
 # pylint: disable=missing-docstring
 import unittest
+from unittest.mock import patch
 from pathlib import Path
 
 from handsdown.utils.path_finder import PathFinder, PathFinderError
 
 
 class TestPathFinder(unittest.TestCase):
+    @patch("handsdown.utils.path_finder.Path")
+    def test_init(self, PathMock):
+        path = PathMock()
+        self.assertIsInstance(PathFinder(path), PathFinder)
+
+        path.is_absolute.return_value = False
+        with self.assertRaises(PathFinderError):
+            self.assertIsInstance(PathFinder(path), PathFinder)
+
+        path.is_absolute.return_value = True
+        path.exists.return_value = True
+        path.is_dir.return_value = False
+        with self.assertRaises(PathFinderError):
+            self.assertIsInstance(PathFinder(path), PathFinder)
+
+        path.is_dir.return_value = True
+        self.assertIsInstance(PathFinder(path), PathFinder)
+
     def test_relative(self):
         path_finder = PathFinder(Path("/root/parent/"))
         self.assertEqual(
